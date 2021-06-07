3f4d8473b1c1630bedf1e79689e549bfa7cde992
Peta Jeannie <peta.jea@onja.com>
2021-06-07 11:38:36 +0300

    Add custom phrases

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -24,6 +24,7 @@ import CheckIcon from '../components/ToolButton/assets/check.svg';
 import CheckAllIcon from '../components/ToolButton/assets/check-all.svg';
 import ModeIcon from '../components/ToolButton/assets/mode.svg';
 import {LEARNT_PRHASES_ID, SEEN_PHRASES_ID} from '../redux/constants/index';
+import { LEARNT_PHRASES_KEY } from '../utils/storage';
 
 export default ({
   navigation,
@@ -39,8 +40,9 @@ export default ({
   // getAllCategoriesAction,
 }) => {
   useEffect(() => {
-    // fetch categories
+    // handle the storing new phrases
     synchronizeStorageToRedux();
+    // fetch categories
     getAllCategories();
   }, []);
 
@@ -61,12 +63,10 @@ export default ({
   };
 
   const openCategoryLearntPhrases = item => {
-    setCurrentCategory(item.id);
-    // fetch Phrases for categor
+    setCurrentCategory(LEARNT_PRHASES_ID);
     setPhrases(learntPhrases);
-    if (learntPhrases.length !== 0) {
-      navigation.navigate('Learn');
-    }
+    learntPhrases.length !== 0 && navigation.navigate('Learn');
+    setCurrentCategory(item.id);
   };
 
   const openSeenPhrases = () => {
